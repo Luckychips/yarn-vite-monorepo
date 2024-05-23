169ecabeb0705b716ddddedc971b86359278413c
10x10toddkim <sungjinkim001@10x10.co.kr>
2024-05-23 11:44:15 +0900

    update monorepo & check

---
+++ apps/managers/src/App.tsx
@@ -0,0 +1,12 @@
+import { Banner } from 'common-ui'
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <Banner instanceName="managers" />
+    </main>
+  )
+}
+
+export default App
--- packages/common-ui/src/components/atoms/Banner/index.tsx
+++ packages/common-ui/src/components/atoms/Banner/index.tsx
@@ -1,4 +1,8 @@
-const Banner = (instanceName: string) => {
+interface ComponentProps {
+    instanceName: string;
+}
+
+const Banner = ({ instanceName }: ComponentProps) => {
     return <h1>Welcome to the {instanceName} portal</h1>;
 };
 
