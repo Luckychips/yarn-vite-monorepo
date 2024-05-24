f259fd8fe4ec1e90e964d0fb41adf915feb94e33
10x10toddkim <sungjinkim001@10x10.co.kr>
2024-05-24 15:36:27 +0900

    update component & add bootstrap

---
+++ apps/admins/src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import { Banner } from 'common-ui'
+  import { useStore } from '../libs/sm'
+  import userStore from '../stores/user'
+  import { print } from '../libs/greeting'
+
+  function change() {
+    print()
+    console.log('change')
+  }
+</script>
+
+<template>
+  <nav class="navbar fixed-top bg-body-tertiary">
+    <div class="container-fluid">
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+</template>
